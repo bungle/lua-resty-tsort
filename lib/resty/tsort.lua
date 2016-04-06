5f15b8b89f4c4d940db935969e27a6ce41607e89
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2016-04-06 13:23:30 +0300

    Small optimizations.

--- lib/resty/tsort.lua
+++ lib/resty/tsort.lua
@@ -4,28 +4,30 @@ local ipairs = ipairs
 local pairs = pairs
 local type = type
 local function visit(k, n, m, s)
-    if m[k] == "temp" then
+    local z = m[k]
+    if z == 0 then
         return "There is a circular dependency in the graph. It is not possible to derive a topological sort."
-    end
-    if m[k] ~= nil then return end
-    m[k] = "temp"
-    for _, y in ipairs(n[k]) do
-        local err = visit(y, n, m, s)
-        if err then
-            return err
+    elseif z ~= 1 then
+        if m[k] ~= nil then return end
+        m[k] = 0
+        for _, y in ipairs(n[k]) do
+            local err = visit(y, n, m, s)
+            if err then
+                return err
+            end
         end
+        m[k] = 1
+        s[#s+1] = k
     end
-    m[k] = "perm"
-    s[#s+1] = k
 end
 local tsort = {}
 tsort.__index = tsort
 function tsort.new()
-    return setmetatable({ nodes = {} }, tsort)
+    return setmetatable({ n = {} }, tsort)
 end
 function tsort:add(...)
     local c = select("#", ...)
-    local n = self.nodes
+    local n = self.n
     local p
     if c == 1 then
         p = select(1, ...)
@@ -51,9 +53,9 @@ function tsort:add(...)
     return self
 end
 function tsort:sort()
+    local n  = self.n
     local s = {}
     local m  = {}
-    local n  = self.nodes
     for k in pairs(n) do
         if m[k] == nil then
             local err = visit(k, n, m, s)
